c7d15715e5e1aa407514b28cac831f92e1adbef4
Suryansh Chandel <suryanshchandel07@gmail.com>
2022-11-30 22:31:24 +0530

    Arithemtic Operators

---
+++ main.py
@@ -0,0 +1,21 @@
+# implementation of operators
+number1 = 19
+number2 = 12
+sum = number1+number2
+print('sum of two number is:',sum)
+
+subtraction = number1-number2
+print('subtraction of two numbers is:',subtraction)
+multiplication = number1*number2
+print('multiplication of two numbers is:',multiplication)
+division = number1/number2
+print('division of two numbers is:',division)
+remainder = number1%number2
+print('remainder of two numbers is:',remainder)
+interdivision = number1/number2
+print('interdivision of two numbers is:',interdivision)
+number1 = 2
+number2 = 3
+power = number2**number1
+print('power of number2 ^ number1 is:',power)
+
